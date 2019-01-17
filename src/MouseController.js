d0b778e4b9dd716027de6568beb21c452e154893
Sebastian <anse23@hotmail.com>
2019-01-17 17:09:54 -0500

    doc: Added comments to generate documentation
    Done for the MouseController.

--- src/MouseController.js
+++ src/MouseController.js
@@ -1,65 +1,135 @@
+/**
+ * Factory function to handle the movement of a mouse.
+ */
 function MouseController() {
     let mMoved = false;
     let mEvent = null;
     let mElement = null;
-
+    /**
+     * Watch the movement of the mouse in a
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Document|document}.
+     *
+     * @param {Document} doc
+     */
     function watchDocument(doc) {
         doc.addEventListener('mousemove', mouseMoveCallback);
     }
-
+    /**
+     * Unwatch the movement of the mouse in a
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Document|document}.
+     *
+     * @param {Document} doc
+     */
     function unwatchDocument(doc) {
         doc.removeEventListener('mousemove', mouseMoveCallback);
     }
-
+    /*
+     * Interal use
+     */
     function mouseMoveCallback(event) {
         mMoved = true;
         mEvent = event;
     }
-
+    /**
+     * Determine if the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}
+     * has been set.
+     *
+     * @returns {boolean}
+     */
     function hasElement() {
         return mElement !== null;
     }
-
+    /**
+     * Reset the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}.
+     */
     function clearElement() {
         mElement = null;
     }
-
+    /**
+     * Set the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}.
+     *
+     * @param {Element} domElement
+     */
     function setElement(domElement) {
         mElement = domElement;
     }
-
+    /**
+     * Get the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}.
+     *
+     * @returns {Element}
+     */
     function getElement() {
         return mElement;
     }
-
-    function setAttrElement(flag, value) {
-        mElement[flag] = value;
+    /**
+     * Set an attribute in the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}.
+     *
+     * @param {string} name
+     * @param {string} value
+     */
+    function setAttrElement(name, value) {
+        mElement[name] = value;
     }
-
-    function getAttrValueElement(flag) {
-        return mElement[flag];
+    /**
+     * Get an attribute's value in the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}.
+     *
+     * @param {string} name
+     *
+     * @returns value
+     */
+    function getAttrValueElement(name) {
+        return mElement[name];
     }
-
+    /**
+     * Determine if the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Element|element}
+     * is the same passed in the argument
+     *
+     * @returns {boolean}
+     */
     function hasThatElement(domElement) {
         return mElement === domElement;
     }
-
+    /**
+     * Set the local flag to determine that the mouse has moved.
+     */
     function move() {
         mMoved = true;
     }
-
+    /**
+     * Set the local flag to determine that the mouse has not moved.
+     */
     function unmove() {
         mMoved = false;
     }
-
+    /**
+     * Get the local flag to determine that the mouse has moved.
+     *
+     * @returns {boolean}
+     */
     function hasMoved() {
         mMoved;
     }
-
+    /**
+     * Determine if the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent|mouse event}
+     * is set.
+     */
     function hasEvent() {
         mEvent !== null;
     }
-
+    /**
+     * Get the local
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent|mouse event}
+     *
+     * @returns {MouseEvent}
+     */
     function getEvent() {
         mEvent;
     }
@@ -67,7 +137,6 @@ function MouseController() {
     return Object.freeze({
         watchDocument,
         unwatchDocument,
-        mouseMoveCallback,
         hasElement,
         clearElement,
         setElement,
