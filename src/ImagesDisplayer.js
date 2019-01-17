c91df95310fb4fadce56bad7a89f26172afa7538
Sebastian <anse23@hotmail.com>
2019-01-17 16:50:55 -0500

    doc: Added comments to generate documentation
    Done for the ImagesDisplayer.

--- src/ImagesDisplayer.js
+++ src/ImagesDisplayer.js
@@ -1,20 +1,42 @@
+/**
+ * Factory function to handle the visibility of images in a
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/Window|window}
+ * and within its
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLIFrameElement|iframes}.
+ */
 function ImagesDisplayer() {
     let mShowAll = false;
     let mIframes = [];
-
+    /**
+     * Set the flag used to show the images.
+     *
+     * @param {boolean} show
+     */
     function setShowAll(show) {
         mShowAll = show;
     }
-
+    /**
+     * Get the flag used to shoe the images.
+     *
+     * @returns {boolean}
+     */
     function isShowAll() {
         return mShowAll;
     }
-
+    /**
+     * Add an
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLIFrameElement|iframe}.
+     *
+     * @param {HTMLIFrameElement}
+     */
     function addIFrame(iframe) {
         mIframes.push(iframe);
     }
     /**
-     * Display images in webpage and iframes
+     * Display images in a
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/Window|window}
+     * and in its
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLIFrameElement|iframes}.
      */
     function showImages() {
         if (mShowAll) {
