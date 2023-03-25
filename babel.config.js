23215d797bc8e2f4b762ce8de3bb6d78d8316093
Sebastian Velasquez <anse23@hotmail.com>
2023-03-25 00:19:55 -0500

    conf: Updates babel for new modules

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 const path = require("path");
 
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     [
-      "babel-plugin-module-resolver",
+      'babel-plugin-module-resolver',
       {
         alias: {
-          Components: path.resolve(__dirname, "./src/js/content/components"),
+          Components: path.resolve(__dirname, './src/js/content/components'),
+          Hooks: path.resolve(__dirname, './src/js/content/hooks'),
+          Utils: path.resolve(__dirname, './src/js/content/utils'),
         },
       },
     ],
