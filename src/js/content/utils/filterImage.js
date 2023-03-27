b358a9f50735b6348748d823c7011b77520b2b08
Sebastian Velasquez <anse23@hotmail.com>
2023-03-27 11:22:22 -0500

    feat: Fastens segmenting to rescale bigger images to 512px

--- src/js/content/utils/filterImage.js
+++ src/js/content/utils/filterImage.js
@@ -45,58 +45,128 @@ function filterSkinColor(imgElement, canvas) {
   return base64Img;
 }
 
-async function segmentPeople(imgElement, canvas) {
-  const model = await bodyPix.load();
+async function grayoutPeople(model, imgElement, canvas, imgElementToGrayout) {
   const person = await model.segmentPerson(imgElement);
 
-  canvas.width = imgElement.width;
-  canvas.height = imgElement.height;
+  if (person.allPoses.length === 0) {
+    throw new Error('No person detected');
+  }
 
   const coloredPartImage = bodyPix.toMask(
     person,
     { r: 127, g: 127, b: 127, a: 255 },
     { r: 0, g: 0, b: 0, a: 0 },
-    false
+    false,
   );
+
+  canvas.width = imgElement.width;
+  canvas.heigh = imgElement.height;
 
   const opacity = 1;
   const flipHorizontal = false;
   const maskBlurAmount = 0;
 
+  const imgToDraw = imgElementToGrayout || imgElement;
+
   bodyPix.drawMask(
     canvas,
-    imgElement,
+    imgToDraw,
     coloredPartImage,
     opacity,
     maskBlurAmount,
-    flipHorizontal
+    flipHorizontal,
   );
+}
 
-  const base64Img = canvas.toDataURL('image/png');
-  return base64Img;
+async function segmentPeople(imgElement, canvas) {
+  const originalImgWidth = imgElement.width;
+  const originalImgHeight = imgElement.height;
+  if (originalImgWidth === 0 || originalImgHeight === 0) {
+    throw new Error('Image width or height is 0');
+  }
+  const model = await bodyPix.load();
+
+  const originalImgRatio = originalImgWidth / originalImgHeight;
+  const originalImgArea = originalImgWidth * originalImgHeight;
+
+  const fasterImgWidth = 512;
+  const fasterImgHeight = Math.ceil(fasterImgWidth / originalImgRatio);
+  const fasterImgArea = fasterImgWidth ** 2;
+
+  // For small enough images, process the original image
+  if (originalImgArea < fasterImgArea) {
+    try {
+      await grayoutPeople(model, imgElement, canvas);
+      return canvas.toDataURL('image/png');
+    } catch (error) {
+      throw new Error('Error graying people out in ORIGINAL image');
+    }
+  }
+
+  // For larger images, resize the image to a smaller size
+  // Image where the segmentation is going to be performed
+  const newImgElement = new Image();
+  newImgElement.width = fasterImgWidth;
+  newImgElement.height = fasterImgHeight;
+  newImgElement.src = imgElement.src;
+
+  // Get the image data to draw the segmentation
+  canvas.width = fasterImgWidth;
+  canvas.height = fasterImgHeight;
+  canvas
+    .getContext('2d')
+    .drawImage(imgElement, 0, 0, fasterImgWidth, fasterImgHeight);
+
+  // Image where the segmentation is going to be drawn
+  const imgElementToApplyGray = new Image();
+  imgElementToApplyGray.width = fasterImgWidth;
+  imgElementToApplyGray.height = fasterImgHeight;
+  imgElementToApplyGray.src = canvas.toDataURL('image/png');
+
+  try {
+    await grayoutPeople(model, newImgElement, canvas, imgElementToApplyGray);
+    // Resize the canvas to the original image size
+    const originalSizedCanvas = document.createElement('canvas');
+    originalSizedCanvas.width = originalImgWidth;
+    originalSizedCanvas.height = originalImgHeight;
+    originalSizedCanvas
+      .getContext('2d')
+      .drawImage(canvas, 0, 0, originalImgWidth, originalImgHeight);
+
+    const base64Img = originalSizedCanvas.toDataURL('image/png');
+    return base64Img;
+  } catch (error) {
+    throw new Error('Error graying people out in RESIZED image');
+  }
 }
 
 export async function processDomImg(imgElement, canvas) {
-  const urlData = await segmentPeople(imgElement, canvas);
-  imgElement.src = urlData;
-  imgElement.srcset = '';
-  imgElement.onload = () => {
+  try {
+    const urlData = await segmentPeople(imgElement, canvas);
+    imgElement.src = urlData;
+    imgElement.srcset = '';
+    imgElement.onload = () => {
+      imgElement.setAttribute('skf-already-processed', 'true');
+      imgElement.classList.remove('skf-hide-class');
+    };
+  } catch (error) {
+    // Either the processing failed or there was no person detected
     imgElement.setAttribute('skf-already-processed', 'true');
     imgElement.classList.remove('skf-hide-class');
-  };
+  }
 }
 
 function fetchAndReadImage(url) {
   return fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
+    .then(response => response.blob())
+    .then(blob => {
       const reader = new FileReader();
       reader.readAsDataURL(blob);
-      return new Promise((resolve, reject) => {
+      return new Promise(resolve => {
         reader.onloadend = () => resolve(reader.result);
       });
     })
-    .then((dataUrl) => {
+    .then(dataUrl => {
       const image = new Image();
       image.crossOrigin = 'anonymous';
       image.src = dataUrl;
@@ -109,7 +179,7 @@ function fetchAndReadImage(url) {
 
 function filterImageElementAsBackground(imgElement, domElement, canvas) {
   const base64Img = filterSkinColor(imgElement, canvas);
-  const newBackgroundImgUrl = "url('" + base64Img + "')";
+  const newBackgroundImgUrl = `url(${base64Img})`;
 
   domElement.style.backgroundImage = newBackgroundImgUrl;
   domElement.setAttribute('skf-already-processed', 'true');
@@ -117,7 +187,7 @@ function filterImageElementAsBackground(imgElement, domElement, canvas) {
 }
 
 export function processBgImg(domElement, bgUrl, canvas) {
-  fetchAndReadImage(bgUrl).then((image) => {
+  fetchAndReadImage(bgUrl).then(image => {
     filterImageElementAsBackground(image, domElement, canvas);
   });
 }
